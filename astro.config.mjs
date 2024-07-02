6c980e7c86041ce0a3fcf3e3d77898602a4aff0e
Onrirr <ulughann@gmail.com>
2024-07-02 10:32:01 +0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://onrirr.github.io/",
+  base: "tuvgan-app",
+  integrations: [
+    starlight({
+      plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "horizontalList",
+          },
+        }),
+      ],
+      title: "Tuvgan Sözlük",
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "Türkçe",
+          direction: "ltr",
+          lang: "tr",
+        },
+        en: {
+          label: "English",
+          direction: "ltr",
+          lang: "en",
+        },
+      },
+      customCss: ["./src/styles/global.css"],
+      sidebar: [
+        {
+          label: "Dictionary",
+          autogenerate: { directory: "./dict/" },
+          translations: {
+            en: "Dictionary",
+            tr: "Sözlük",
+            uz: "Lug'at",
+          },
+        },
+      ],
+    }),
+  ],
+});
